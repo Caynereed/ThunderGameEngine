8b087e0c163119b0b13f07b9d848c9c32778056c
eecemath <buschdr@gmail.com>
2023-11-05 08:53:19 -0600

    Updated with premake

--- ThunderGameEngine/src/Thunder.h
+++ ThunderGameEngine/src/Thunder.h
@@ -3,6 +3,7 @@
 // For use by Thunder applications
 
 #include "Thunder/Application.h"
+#include "Thunder/Log.h"
 
 // ---Entry Point---------------------
 #include "Thunder/EntryPoint.h"
--- ThunderGameEngine/src/Thunder/EntryPoint.h
+++ ThunderGameEngine/src/Thunder/EntryPoint.h
@@ -6,6 +6,10 @@ extern Thunder::Application* Thunder::CreateApplication();
 
 void main(int argc, char** argv)
 {
+	Thunder::Log::Init();
+	TD_CORE_TRACE("Initialized Log!");
+	TD_INFO("Initialized Log!");
+
 	auto app = Thunder::CreateApplication();
 	app->Run();
 	delete app;
